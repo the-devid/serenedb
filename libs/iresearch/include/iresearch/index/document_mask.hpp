a4b8283f2f29d422d2b8250f258c740a0d97fdb3
Denis Vidiaev <the.devid@yandex.ru>
2026-04-26 21:47:57 +0900

    run formatter on document_mask.hpp

--- libs/iresearch/include/iresearch/index/document_mask.hpp
+++ libs/iresearch/include/iresearch/index/document_mask.hpp
@@ -1,15 +1,14 @@
 #pragma once
 
-
-#include "basics/managed_allocator.hpp"
-#include "basics/resource_manager.hpp"
-#include "basics/containers/bitset.hpp"
-#include "iresearch/types.hpp"
-#include "iresearch/utils/type_limits.hpp"
-
 #include <absl/container/flat_hash_set.h>
 
 #include <cstddef>
+
+#include "basics/containers/bitset.hpp"
+#include "basics/managed_allocator.hpp"
+#include "basics/resource_manager.hpp"
+#include "iresearch/types.hpp"
+#include "iresearch/utils/type_limits.hpp"
 
 namespace irs {
 
@@ -24,33 +23,28 @@ class DocumentMask {
   bool operator==(const DocumentMask& other) const {
     bool equal = (DeletedDocCount() == other.DeletedDocCount());
     ForEach([&equal, &other](doc_id_t doc_id) {
-        equal = equal && other.IsDeleted(doc_id);
+      equal = equal && other.IsDeleted(doc_id);
     });
     return equal;
   }
-  bool IsEmpty() const {
-    return DeletedDocCount() == 0;
-  }
+  bool IsEmpty() const { return DeletedDocCount() == 0; }
 };
-
 
 class DocumentHashMask final : public DocumentMask {
  public:
   DocumentHashMask() = default;
-  explicit DocumentHashMask(IResourceManager& rm) : stored_docs_{ManagedTypedAllocator<doc_id_t>{rm}} {}
-  DocumentHashMask(IResourceManager& rm, const DocumentMask& other) : stored_docs_{ManagedTypedAllocator<doc_id_t>{rm}} {
+  explicit DocumentHashMask(IResourceManager& rm)
+    : stored_docs_{ManagedTypedAllocator<doc_id_t>{rm}} {}
+  DocumentHashMask(IResourceManager& rm, const DocumentMask& other)
+    : stored_docs_{ManagedTypedAllocator<doc_id_t>{rm}} {
     HintDeletedDocCount(other.DeletedDocCount());
-    other.ForEach([this](doc_id_t doc_id) {
-      MarkDeleted(doc_id);
-    });
+    other.ForEach([this](doc_id_t doc_id) { MarkDeleted(doc_id); });
   }
 
   bool IsDeleted(doc_id_t doc_id) const override {
-      return stored_docs_.contains(doc_id);
+    return stored_docs_.contains(doc_id);
   }
-  size_t DeletedDocCount() const override {
-      return stored_docs_.size();
-  }
+  size_t DeletedDocCount() const override { return stored_docs_.size(); }
   void ForEach(const std::function<void(doc_id_t)>& cb) const override {
     for (auto doc_id : stored_docs_) {
       cb(doc_id);
@@ -60,40 +54,36 @@ class DocumentHashMask final : public DocumentMask {
   bool MarkDeleted(doc_id_t doc_id) {
     return stored_docs_.insert(doc_id).second;
   }
-  void HintDeletedDocCount(size_t count) {
-    stored_docs_.reserve(count);
-  }
+  void HintDeletedDocCount(size_t count) { stored_docs_.reserve(count); }
   void Merge(DocumentHashMask& other) {
     stored_docs_.merge(other.stored_docs_);
   }
-  void Clear() {
-    stored_docs_.clear();
-  }
+  void Clear() { stored_docs_.clear(); }
 
  private:
   absl::flat_hash_set<doc_id_t,
-    absl::container_internal::hash_default_hash<doc_id_t>,
-    absl::container_internal::hash_default_eq<doc_id_t>,
-    ManagedTypedAllocator<doc_id_t>> stored_docs_;
+                      absl::container_internal::hash_default_hash<doc_id_t>,
+                      absl::container_internal::hash_default_eq<doc_id_t>,
+                      ManagedTypedAllocator<doc_id_t>>
+    stored_docs_;
 };
 
 class DocumentBitMask final : public DocumentMask {
  public:
   DocumentBitMask() = default;
-  explicit DocumentBitMask(IResourceManager& rm) : is_deleted_{rm}, deleted_doc_count_{0} {}
-  DocumentBitMask(IResourceManager& rm, size_t doc_count, const DocumentMask& other) : is_deleted_{rm}, deleted_doc_count_{other.DeletedDocCount()} {
+  explicit DocumentBitMask(IResourceManager& rm)
+    : is_deleted_{rm}, deleted_doc_count_{0} {}
+  DocumentBitMask(IResourceManager& rm, size_t doc_count,
+                  const DocumentMask& other)
+    : is_deleted_{rm}, deleted_doc_count_{other.DeletedDocCount()} {
     is_deleted_.resize(doc_count + doc_limits::min());
-    other.ForEach([this](doc_id_t doc_id) {
-      is_deleted_.set(doc_id);
-    });
+    other.ForEach([this](doc_id_t doc_id) { is_deleted_.set(doc_id); });
   }
 
   bool IsDeleted(doc_id_t doc_id) const override {
     return is_deleted_.test(doc_id);
   }
-  size_t DeletedDocCount() const override {
-    return deleted_doc_count_;
-  }
+  size_t DeletedDocCount() const override { return deleted_doc_count_; }
   void ForEach(const std::function<void(doc_id_t)>& cb) const override {
     // TODO: add FindNext-like method to DynamicBitset and optimize this loop
     for (doc_id_t doc_id = 0; doc_id < is_deleted_.size(); ++doc_id) {
@@ -102,9 +92,11 @@ class DocumentBitMask final : public DocumentMask {
       }
     }
   }
+
  private:
   ManagedBitset is_deleted_;
-  size_t deleted_doc_count_{0}; // to not run popcount each DeletedDocCount call
+  size_t deleted_doc_count_{
+    0};  // to not run popcount each DeletedDocCount call
 };
 
 }  // namespace irs
