595a1968619894c102e133d70e58196e9930f8e3
Denis Vidiaev <the.devid@yandex.ru>
2026-04-26 21:43:43 +0900

    add DocumentMask impl with bitset

--- libs/iresearch/include/iresearch/index/document_mask.hpp
+++ libs/iresearch/include/iresearch/index/document_mask.hpp
@@ -77,4 +77,34 @@ class DocumentHashMask final : public DocumentMask {
     ManagedTypedAllocator<doc_id_t>> stored_docs_;
 };
 
+class DocumentBitMask final : public DocumentMask {
+ public:
+  DocumentBitMask() = default;
+  explicit DocumentBitMask(IResourceManager& rm) : is_deleted_{rm}, deleted_doc_count_{0} {}
+  DocumentBitMask(IResourceManager& rm, size_t doc_count, const DocumentMask& other) : is_deleted_{rm}, deleted_doc_count_{other.DeletedDocCount()} {
+    is_deleted_.resize(doc_count + doc_limits::min());
+    other.ForEach([this](doc_id_t doc_id) {
+      is_deleted_.set(doc_id);
+    });
+  }
+
+  bool IsDeleted(doc_id_t doc_id) const override {
+    return is_deleted_.test(doc_id);
+  }
+  size_t DeletedDocCount() const override {
+    return deleted_doc_count_;
+  }
+  void ForEach(const std::function<void(doc_id_t)>& cb) const override {
+    // TODO: add FindNext-like method to DynamicBitset and optimize this loop
+    for (doc_id_t doc_id = 0; doc_id < is_deleted_.size(); ++doc_id) {
+      if (is_deleted_.test(doc_id)) {
+        cb(doc_id);
+      }
+    }
+  }
+ private:
+  ManagedBitset is_deleted_;
+  size_t deleted_doc_count_{0}; // to not run popcount each DeletedDocCount call
+};
+
 }  // namespace irs
